dfe2c244985be56c59aabc4edc490269fe6e1445
pawni mishra <pawnim37@gmail.com>
2026-09-25 22:50:19 +0530

    Time: 6 ms (79.37%), Space: 51.2 MB (41.45%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+
+        int totalSum = 0;
+
+        int currentMax = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        int currentMin = 0;
+        int minSum = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            // Maximum subarray (Kadane)
+            currentMax = Math.max(num, currentMax + num);
+            maxSum = Math.max(maxSum, currentMax);
+
+            // Minimum subarray
+            currentMin = Math.min(num, currentMin + num);
+            minSum = Math.min(minSum, currentMin);
+
+            totalSum += num;
+        }
+
+        // All elements are negative
+        if (maxSum < 0) {
+            return maxSum;
+        }
+
+        // Maximum circular subarray
+        int circularSum = totalSum - minSum;
+
+        return Math.max(maxSum, circularSum);
+    }
+}
